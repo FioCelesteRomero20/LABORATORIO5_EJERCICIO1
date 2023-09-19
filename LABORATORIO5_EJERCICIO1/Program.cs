b9c3999a295baa9a6ad75db56aa40cf2e039ad13
PC <PC@DESKTOP-NOE8GP1>
2023-09-19 17:49:34 -0500

    Agregar archivos de proyecto.

---
+++ LABORATORIO5_EJERCICIO1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABORATORIO5_EJERCICIO1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el primer numero: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo numero: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int resultado = 0;
+            for (int i = 0; i < num2; i++)
+            {
+                resultado += num;
+            }
+
+            Console.WriteLine("El resultado es: " + resultado);
+            Console.ReadKey();
+        }
+    }
+}
